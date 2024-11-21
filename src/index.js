a4bbb3619d32a9cb3319c6de976314730a5b6b42
andronicosx <61975005+andronicos23@users.noreply.github.com>
2024-11-21 11:38:13 +0200

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import React from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import App from "./App";
+import "./index.css";
+import App from "./App";
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating colo="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>The Rating was set on {movieRating} for this movie.</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} color={"#fcc419"} size={20} />
-    <StarRating maxRating={5} color="red" size={30} />
+    <StarRating
+      maxRating={5}
+      color={"#fcc419"}
+      size={20}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <StarRating maxRating={5} color="red" size={30} className="test" />
+    <Test />
   </React.StrictMode>
 );
